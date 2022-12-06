3af6c70f1f7e51316ee3a3bd4d16c0347629ff08
jibundeyare <contact@jibundeyare.com>
2022-12-06 12:06:22 +0100

    Ajout d'un cours sur les opérateurs de comparaison

--- operators.py
+++ operators.py
@@ -11,12 +11,18 @@ foo = foo + 42
 foo += 42
 print(foo)
 
+# foo++ équivalent de foo += 1
+# mais l'opérateur ++ n'existe pas en python
+
 # - soustraction
 foo = 123 - 42
 foo = foo - 42
 # -= opérateur de décrémentation
 foo -= 42
 print(foo)
+
+# foo-- équivalent de foo -= 1
+# mais l'opérateur -- n'existe pas en python
 
 # / division
 foo = 123 / 42
@@ -57,3 +63,33 @@ foo = 4 ** (1/2)
 # racine cubique
 foo = 8 ** (1/3)
 print(foo)
+
+# les opérateurs de comparaison
+
+# l'égalité ==
+# à ne pas confondre avec l'affectation =
+# à ne pas confondre avec l'identité === (qui n'existe pas en python)
+result = 1 == 1.0
+print(result)
+
+# les grandeurs
+# plus petit que
+result = 123 < 42
+print(result)
+
+# plus petit ou égal à
+# à ne pas confondre avec => js
+result = 123 <= 42
+print(result)
+
+# plus grand que
+result = 123 > 42
+print(result)
+
+# plus grand ou égal à
+result = 123 >= 42
+print(result)
+
+# l'inégalité (ou différence)
+result = 123 != 42
+print(result)
