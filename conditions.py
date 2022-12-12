00d53a3fa6bf3c110c1f342a8b77bcd5e05c52e3
jibundeyare <contact@jibundeyare.com>
2022-12-12 16:56:22 +0100

    Suite du cours

--- conditions.py
+++ conditions.py
@@ -50,3 +50,58 @@ elif windows_closed and not electricity_off:
 else:
     print("Les fenêtres ne sont pas fermées")
     print("L'électricité n'est pas coupée")
+
+bank_card = True
+cash = True
+
+print(f'{bank_card = }')
+print(f'{cash = }')
+
+if bank_card or cash:
+    print("On a au moins un moyen de paiement")
+
+    if bank_card:
+        print("On a la carte bancaire")
+
+    if cash:
+        print("On a du liquide")
+else:
+    print("On a aucun moyen de paiement")
+
+ticket = bool(random.randint(0, 1))
+vip = bool(random.randint(0, 1))
+registration = bool(random.randint(0, 1))
+
+print(f'{ticket = }')
+print(f'{vip = }')
+print(f'{registration = }')
+
+# Quand on mélange or et and, il faut toujours utiliser les parenthèses 
+if (ticket or vip) and registration:
+    print("Access authorized")
+else:
+    print("Access not authorized")
+
+product_count = random.randint(0, 50)
+
+print(f'{product_count = }')
+
+if product_count > 20:
+    print("Il y a plus de 20 articles")
+    print("RAS")
+elif 5 < product_count <= 20:
+    print("Il y a plus de 5 articles")
+    print("Alerte approvisionnement")
+elif 0 < product_count <= 5:
+    print("Il y a plus de 0 articles")
+    print("Alerte rupture imminente")
+else:
+    print("Il n'y a plus d'article")
+    print("Alerte rupture")
+
+product_count = 6
+
+# équivalent d'un encadrement python
+if product_count > 5 and product_count <= 20:
+    print("Il y a plus de 5 articles")
+    print("Il y a 20 ou moins d'articles")
