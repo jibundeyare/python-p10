0e17d69bb02b37f773da77a028d6cf5ea73cff57
jibundeyare <contact@jibundeyare.com>
2022-12-13 16:27:13 +0100

    Suite du cours

--- lists.py
+++ lists.py
@@ -33,3 +33,30 @@ index += 1
 if index < my_count:
     print(f'{index = }')
     print(fruits[index])
+
+# ajouter un élément
+fruits = ['ananas', 'banane', 'cerise', 'mangue']
+fruits.append('datte')
+print(fruits)
+
+# supprimer un élément sans le récupérer
+del fruits[0]
+print(fruits)
+
+fruits.remove(0)
+print(fruits)
+
+# supprime et renvoie le dernier élément
+last_element = fruits.pop()
+print(fruits)
+print(last_element)
+
+# supprime et renvoie le premier élément
+# pop(0) est équivalent à shift()
+first_element = fruits.pop(0)
+print(fruits)
+print(first_element)
+
+# insérer un élément
+fruits.insert(1, 'kiwi')
+print(fruits)
