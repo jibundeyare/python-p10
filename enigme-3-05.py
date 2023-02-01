dcda9cf15fc1da071266220cd1b9f21388d5bf61
jibundeyare <contact@jibundeyare.com>
2023-02-01 19:08:33 +0100

    Ajout d'une énigme

---
+++ enigme-3-05.py
@@ -0,0 +1,7 @@
+# 5. À la queue leu-leu
+#
+# Pour savoir jusqu'où elle sait compter, la petite Azélie écrit sur une
+# feuille de papier tous les entiers à partir de 1 et à la queue leu-leu :
+# 123456789101112131415 etc. On voit par exemple que dans cette liste,
+# arrive en 20ème position le chiffre un.
+# Quel chiffre occupe la 2018ème position ?
